aaf66d0a8dc9cc093e6216880248e6e56f74f090
Mark Gibson <jollytoad@gmail.com>
2009-05-07 17:42:43 +0100

    Added pre events that can be cancelled.

--- src/jquery.mutations.js
+++ src/jquery.mutations.js
@@ -12,6 +12,10 @@
 var m;
 m = $.mutations = {
 
+	REMOVAL:		3,
+	ADDITION:		2,
+	MODIFICATION:	1,
+
 	// Construct a mutation event
 	event: function( eventType, attrName, prevValue, newValue ) {
 		var event = new $.Event( eventType );
@@ -22,121 +26,123 @@ m = $.mutations = {
 		event.prevValue = prevValue;
 		
 		event.attrChange =
-			newValue == null ? 3 :	// REMOVAL
-			prevValue == null ? 2 :	// ADDITION
-			1;						// MODIFICATION
+			newValue === null || newValue === undefined   ? m.REMOVAL :
+			prevValue === null || prevValue === undefined ? m.ADDITION :
+															m.MODIFICATION;
 		
 		return event;
 	},
 	
-	// Construct and trigger a mutation event, returning the event
-	trigger: function( elem, eventType, attrName, prevValue, newValue ) {
-		var event = m.event(eventType, attrName, prevValue, newValue);
-//		console.log('trigger %s %s: %o -> %o %o', event.type, event.attrName, event.prevValue, event.newValue, elem);
+	trigger: function( elem, eventType, attrName, prevValue, newValue, commit ) {
+		var event = m.event('pre-' + eventType, attrName, prevValue, newValue),
+			ret;
+
+		console.log('trigger %s %s: %o -> %o %o', event.type, event.attrName, event.prevValue, event.newValue, elem);
+
 		$.event.trigger( event, undefined, elem );
-		return event;
+		
+		if ( !event.isDefaultPrevented() ) {
+			event.type = eventType;
+			ret = commit(event);
+			$.event.trigger( event, undefined, event.target );
+		}
+		
+		return ret;
 	},
 	
-//	trigger: function( elem, eventType, attrName, prevValue, newValue, commit ) {
-//		var event = m.event(eventType, 'pre-' + attrName, prevValue, newValue);
-////		console.log('trigger %s %s: %o -> %o %o', event.type, event.attrName, event.prevValue, event.newValue, elem);
-
-//		$.event.trigger( event, undefined, elem );
-//		
-//		if ( !event.isDefaultPrevented() ) {
-//			event.attrName = attrName;
-//			event.newValue = commit(event);
-//			$.event.trigger( event, undefined, event.target );
-//		}
-//		return event;
-//	},
+	// Items which should never have mutation events triggered
+	blacklist: {
+		attr: {},
+		data: { 'events':true, 'handle':true, 'olddisplay':true }
+	},
 	
-	_attr: $.attr,
-	_data: $.data,
-	_removeData: $.removeData,
-	_fn_val: $.fn.val
-};
+	_commit: function( event ) {
+		m._$[event.type]( event.target, event.attrName, event.newValue );
+	},
+	
+	_$: {
+		attr: $.attr,
+		data: $.data,
+		removeData: $.removeData
+	},
+	
+	_fn: {
+		val: $.val
+	}
+
+}; // $.mutation
+
 
 // Override jQuery.* function to trigger mutation events when values are changed
 // The additional silent argument allows a change without triggering
 $.extend({
 
 	attr: function( elem, name, newValue, silent ) {
-		var prevValue = m._attr( elem, name );
+		var prevValue = m._$.attr( elem, name );
 
 		if ( newValue === undefined ) {
 			return prevValue;
 		}
 
-		if ( silent ) {
-			return m._attr( elem, name, newValue );
-			
+		if ( silent || m.blacklist.attr[name] ) {
+			return m._$.attr( elem, name, newValue );
 		} else if ( ""+newValue !== ""+prevValue ) {
-			var event = m.trigger( elem, 'attr', name, prevValue, newValue === "" ? undefined : newValue );
-			
-			// Don't perform the actual change if preventDefault was called
-			if ( !event.isDefaultPrevented() ) {
-				return m._attr( event.target, event.attrName, event.newValue );
-			}
+			return m.trigger( elem, 'attr', name, prevValue, newValue === "" ? undefined : newValue, m._commit );
 		}
 	},
 	
 	data: function( elem, name, newData, silent ) {
-		var prevData = m._data( elem, name );
+		var prevData = m._$.data( elem, name );
 		
 		if ( newData === undefined ) {
 			return prevData;
 		}
 		
-		if ( silent ) {
-			return m._data( elem, name, newData );
-			
+		if ( silent || m.blacklist.data[name] ) {
+			return m._$.data( elem, name, newData );
 		} else if ( newData !== prevData ) {
-			var event = m.trigger( elem, 'data', name, prevData, newData );
-			
-			if ( !event.isDefaultPrevented() ) {
-				return m._data( event.target, event.attrName, event.newValue );
-			}
+			return m.trigger( elem, 'data', name, prevData, newData, m._commit );
 		}
 	},
 	
 	// Override jQuery.removeData to trigger mutation 
 	removeData: function( elem, name, silent ) {
-		if ( silent ) {
-			return m._removeData( elem, name );
+		if ( silent || m.blacklist.data[name] ) {
+			return m._$.removeData( elem, name );
 		}
-		
-		var event = m.trigger( elem, 'data', name, m._data(elem, name) );
-		
-		if ( !event.isDefaultPrevented() ) {
-			if ( event.attrChange === 3 ) {
-				return m._removeData( event.target, event.attrName );
-			} else {
-				// The event handler wants the data modified rather than removed
-				return m._data( event.target, event.attrName, event.newValue );
-			}
-		}
+
+		return m.trigger( elem, 'data', name, m._$.data(elem, name), undefined,
+			function(event) {
+				if ( event.attrChange === m.REMOVAL ) {
+					m._$.removeData( event.target, event.attrName );
+				} else {
+					// The event handler wants the data modified rather than removed
+					m._$.data( event.target, event.attrName, event.newValue );
+				}
+			});
 	}
-});
+
+}); // $.extend
+
 
 // Override jQuery.fn.* function to trigger mutation events when values are changed
 $.fn.extend({
 	
 	val: function( newValue, silent ) {
 		if ( newValue === undefined ) {
-			return m._fn_val.apply(this);
+			return m._fn.val.apply(this);
 		}
 		
 		if ( silent ) {
-			return m._fn_val.call( this, newValue );
+			return m._fn.val.call( this, newValue );
 		}
 		
 		return this.each(function() {
-			var event = m.trigger( this, 'val', 'val', newValue, m._fn_val.apply([this]) );
-			
-			if ( !event.isDefaultPrevented() ) {
-				m._fn_val.call( $(event.target), event.newValue );
-			}
+			m.trigger( this, 'val', 'val', newValue, m._fn.val.apply([this]),
+				function(event) {
+					m._fn.val.call( $(event.target), event.newValue );
+				}
+			);
 		});
 	},
 	
@@ -145,15 +151,16 @@ $.fn.extend({
 		return this.each(function() {
 			var elem = this;
 			$.each(names.split(/\s+/), function(n, name) {
-				var val = m['_'+type] ? m['_'+type](elem) : m['_fn_'+type].apply(elem);
+				var val = m._$[type] ? m._$[type](elem) : m._fn[type].apply(elem);
 				if ( val !== undefined ) {
-					m.trigger( elem, type, name, undefined, val );
+					$.trigger(m.event(type, name, undefined, val), undefined, elem);
 				}
 			});
 		});
 	}
 
-});
+}); // $.fn.extend
+
 
 })(jQuery)
 );
